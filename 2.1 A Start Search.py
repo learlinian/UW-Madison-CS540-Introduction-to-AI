84313c5aca367c9583b5d21e87aea5e74997d7fa
Nian <40905820+learlinian@users.noreply.github.com>
2019-02-16 11:15:30 +0800

    Add files via upload
    Use A* search method to find visited nodes and optimal sequence of graph.

---
+++ 2.1 A Start Search.py
@@ -0,0 +1,67 @@
+tree = {'S': [['A', 1], ['B', 5], ['C', 8]],
+        'A': [['S', 1], ['D', 3], ['E', 7], ['G', 9]],
+        'B': [['S', 5], ['G', 4]],
+        'C': [['S', 8], ['G', 5]],
+        'D': [['A', 3]],
+        'E': [['A', 7]]}
+
+tree2 = {'S': [['A', 1], ['B', 2]],
+         'A': [['S', 1]],
+         'B': [['S', 2], ['C', 3], ['D', 4]],
+         'C': [['B', 2], ['E', 5], ['F', 6]],
+         'D': [['B', 4], ['G', 7]],
+         'E': [['C', 5]],
+         'F': [['C', 6]]
+         }
+
+heuristic = {'S': 8, 'A': 8, 'B': 4, 'C': 3, 'D': 5000, 'E': 5000, 'G': 0}
+heuristic2 = {'S': 0, 'A': 5000, 'B': 2, 'C': 3, 'D': 4, 'E': 5000, 'F': 5000, 'G': 0}
+
+cost = {'S': 0}             # total cost for nodes visited
+
+
+def AStarSearch():
+    global tree, heuristic
+    closed = []             # closed nodes
+    opened = [['S', 8]]     # opened nodes
+
+    '''find the visited nodes'''
+    while True:
+        fs = [i[1] for i in opened]     # fs = f(s) = g(s) + h(s)
+        chosen_index = fs.index(min(fs))
+        node = opened[chosen_index][0]  # current node
+        closed.append(opened[chosen_index])
+        del opened[chosen_index]
+        if closed[-1][0] == 'G':        # break the loop if node G has been found
+            break
+        for item in tree[node]:
+            if item[0] in [closed_item[0] for closed_item in closed]:
+                continue
+            cost.update({item[0]: cost[node] + item[1]})            # add nodes to cost dictionary
+            fs_node = cost[node] + heuristic[item[0]] + item[1]     # calculate f(s) of current node
+            temp = [item[0], fs_node]
+            opened.append(temp)                                     # store f(s) of current node in array opened
+
+    '''find optimal sequence'''
+    trace_node = 'G'                        # correct optimal tracing node, initialize as node G
+    optimal_sequence = ['G']                # optimal node sequence
+    for i in range(len(closed)-2, -1, -1):
+        check_node = closed[i][0]           # current node
+        if trace_node in [children[0] for children in tree[check_node]]:
+            children_costs = [temp[1] for temp in tree[check_node]]
+            children_nodes = [temp[0] for temp in tree[check_node]]
+
+            '''check whether h(s) + g(s) = f(s). If so, append current node to optimal sequence
+            change the correct optimal tracing node to current node'''
+            if cost[check_node] + children_costs[children_nodes.index(trace_node)] == cost[trace_node]:
+                optimal_sequence.append(check_node)
+                trace_node = check_node
+    optimal_sequence.reverse()              # reverse the optimal sequence
+
+    return closed, optimal_sequence
+
+
+if __name__ == '__main__':
+    visited_nodes, optimal_nodes = AStarSearch()
+    print('visited nodes: ' + str(visited_nodes))
+    print('optimal nodes sequence: ' + str(optimal_nodes))
